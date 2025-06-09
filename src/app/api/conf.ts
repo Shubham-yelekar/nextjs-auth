5e9d987842aa063d2cc8e45cd322c042bac75b0e
shubham <shubhamyelekar0@gmail.com>
2025-06-09 13:31:47 +0530

    mailer

--- src/app/api/conf.ts
+++ src/app/api/conf.ts
@@ -1,6 +1,8 @@
 const conf = {
   mongo_uri: String(process.env.MONGO_URI),
   secret_token: String(process.env.TOKEN_SECRET),
+  nodemailer_user: String(process.env.NODEMAILER_USER),
+  nodemailer_pass: String(process.env.NODEMAILER_PASS),
 };
 
 export default conf;
--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,7 @@ import { connect } from "@/app/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-
+import { sendEmail } from "@/helpers/mailer";
 connect();
 console.log("Database connected successfully.");
 
@@ -31,6 +31,8 @@ export async function POST(request: NextRequest) {
     });
 
     const savedUser = await newUser.save();
+
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
 
     return NextResponse.json({
       message: "User created successfully",
